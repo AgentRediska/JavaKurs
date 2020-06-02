69b1f9f3978b79be96a25dbe0d9bf45f0b151802
Agent Rediska <Agent_Rediska>
2020-06-02 23:07:22 +0300

    Создание файлов для хранения данных о диванах

--- src/furniture/Start.java
+++ src/furniture/Start.java
@@ -4,6 +4,6 @@ import Windows.BuildGui;
 public abstract class Start {
 
     public static void main(String[] args) {
-        new BuildGui().buildGUI();  //запуск создания окна
+        new BuildGui().buildGUI();  //запуск создания окна/
     }
 }
